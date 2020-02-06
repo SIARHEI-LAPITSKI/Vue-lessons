8d6c13e187bc115bc693634a7600adf93cfc30f7
Lapitski <s.lapitskij@gmail.com>
2020-02-06 22:01:27 +0300

    Final note app
    End up note app project whihoud HW

--- noteApp/src/App.vue
+++ noteApp/src/App.vue
@@ -4,27 +4,44 @@
         <div class="wrapper-content">
             <section>
                 <div class="container">
-                    <h1>{{title}}</h1>
+                   
+                    <!-- message -->
+                    <message 
+                        v-if="message" 
+                        :message="message" 
+                    />
 
-                    <message v-if="message" v-bind:message="message" />
-                    <!--note note-->
-                    <div class="new-note">
-                        <input v-model="note.title" type="text">
-                        <textarea v-model="note.descr"></textarea>
-                        <button @click="addNote">New note</button>
+                    <!-- new note -->
+                    <NewNote 
+                        :note="note" 
+                        @addNote="addNote" 
+                    />
+                    
+                    
+                    <div class="note-header">
+                        <!-- title  -->
+                        <h1>{{title}}</h1>
+
+                        <!-- search -->
+                        <Search 
+                            :value="search"
+                            placeholder="Find your note"
+                            @search="search = $event"
+                        />
+
+                        <!-- icons controls -->
+                        <div class="icons">
+                            <svg :class="{ active: grid }" @click="grid = true" style="cursor: pointer;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
+                            <svg :class="{ active: !grid }" @click="grid = false" style="cursor: pointer;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg>
+                        </div>
                     </div>
 
-                    <!--note list-->
-                    <div class="notes" v-for="(note, index) in notes" :key="index">
-                        <div class="note-header">
-                            <p>{{ note.title }}</p>
-                        </div>
-                        <div class="note-body">
-                            <p>{{ note.description }}</p>
-                            <span>{{ note.date }}</span>
-                        </div>
-
-                    </div>
+                    <!-- note list -->
+                    <Notes 
+                        :notes="notesFilter"
+                        @remove="removeNote"
+                        :grid="grid"
+                    />
                 </div>
             </section>
         </div>
@@ -33,17 +50,23 @@
 </template>
 
 <script>
-    import message from '@/components/Message.vue'
-    import Message from "./components/Message";
+    import Message from "@/components/Message";
+    import NewNote from "@/components/NewNote";
+    import Notes from "@/components/Notes";
+    import Search from "@/components/Search";
     export default {
-        components: {Message},
-        component: {
-            message,
+        components: {
+            Message,
+            NewNote,
+            Notes,
+            Search
         },
         data() {
             return {
                 title: 'Nodes App',
+                search: '',
                 message: null,
+                grid: true,
                 note: {
                     title: '',
                     descr: ''
@@ -67,6 +90,23 @@
                 ]
             }
         },
+        computed: { 
+            notesFilter(){
+                let array = this.notes,
+                    search = this.search
+                if(!search) return array
+                //small
+                search = search.trim().toLowerCase()
+                //filter
+                array = array.filter(function(item){
+                    if(item.title.toLowerCase().indexOf(search) !== -1){
+                        return item
+                    }
+                })
+                //Error
+                return array
+            }
+        },
         methods: {
             addNote() {
                 // console.log(this.note)
@@ -83,6 +123,9 @@
                 this.message = null;
                 this.note.title = '';
                 this.note.descr = '';
+            },
+            removeNote (index){
+                this.notes.splice(index, 1)
             }
         }
     }
