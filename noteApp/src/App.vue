a97f3c7996b4cd7955f061200b5981570a8d3fce
Sergey Lapitski <s.lapitskij@gmail.com>
2020-02-06 17:56:08 +0300

    finish workday Vue course

--- noteApp/src/App.vue
+++ noteApp/src/App.vue
@@ -1,47 +1,91 @@
 <template>
-  <div class="wrapper">
+    <div class="wrapper">
 
-    <div class="wrapper-content">
-      <section>
-        <div class="container">
-          <h1>{{title}}</h1>
+        <div class="wrapper-content">
+            <section>
+                <div class="container">
+                    <h1>{{title}}</h1>
 
-          <div class="message" v-if="message">
-            <p>{{ message }}</p>
-          </div>
-          <!--note note-->
-          <div class="new-note">
-            <input v-model="note.title" type="text">
-            <textarea v-model="note.descr"></textarea>
-            <button @click="addNote">New note</button>
-          </div>
+                    <message v-if="message" v-bind:message="message" />
+                    <!--note note-->
+                    <div class="new-note">
+                        <input v-model="note.title" type="text">
+                        <textarea v-model="note.descr"></textarea>
+                        <button @click="addNote">New note</button>
+                    </div>
 
-          <!--note list-->
-          <div class="notes" v-for="(note, index) in notes" :key="index">
-            <div class="note-header">
-              <p>{{ note.title }}</p>
-            </div>
-            <div class="note-body">
-              <p>{{ note.description }}</p>
-              <span>{{ note.date }}</span>
-            </div>
+                    <!--note list-->
+                    <div class="notes" v-for="(note, index) in notes" :key="index">
+                        <div class="note-header">
+                            <p>{{ note.title }}</p>
+                        </div>
+                        <div class="note-body">
+                            <p>{{ note.description }}</p>
+                            <span>{{ note.date }}</span>
+                        </div>
 
-          </div>
+                    </div>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
 
-  </div>
+    </div>
 </template>
 
 <script>
-export default {
-  data (){
-    return {
-
+    import message from '@/components/Message.vue'
+    import Message from "./components/Message";
+    export default {
+        components: {Message},
+        component: {
+            message,
+        },
+        data() {
+            return {
+                title: 'Nodes App',
+                message: null,
+                note: {
+                    title: '',
+                    descr: ''
+                },
+                notes: [
+                    {
+                        title: 'First note',
+                        description: 'Description for first note',
+                        date: new Date(Date.now()).toLocaleString()
+                    },
+                    {
+                        title: 'Second note',
+                        description: 'Description for second note',
+                        date: new Date(Date.now()).toLocaleString()
+                    },
+                    {
+                        title: 'Third note',
+                        description: 'Description for third note',
+                        date: new Date(Date.now()).toLocaleString()
+                    },
+                ]
+            }
+        },
+        methods: {
+            addNote() {
+                // console.log(this.note)
+                let {title, descr} = this.note;
+                if (title === '') {
+                    this.message = 'title cannot be blank';
+                    return false;
+                }
+                this.notes.push({
+                    title: title,
+                    description: descr,
+                    date: new Date(Date.now()).toLocaleString()
+                });
+                this.message = null;
+                this.note.title = '';
+                this.note.descr = '';
+            }
+        }
     }
-  }
-}
 </script>
 
 <style>
